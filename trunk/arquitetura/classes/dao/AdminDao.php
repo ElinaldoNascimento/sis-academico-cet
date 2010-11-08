6b128a1b0514c222475664a391cd071d9e8e5c38
vinicius.wd1 <vinicius.wd1@c1a4cfde-91b5-6961-b273-67d140422e45>
2010-11-08 22:57:54 +0000

    Adição do método "adicionar" para adicionar usuário administrador

--- trunk/arquitetura/classes/dao/AdminDao.php
+++ trunk/arquitetura/classes/dao/AdminDao.php
@@ -15,7 +15,19 @@ class AdminDao {
     public function  __construct() {
         $this->connection = Conexao::getConnection();
     }
-
+    public function adiciona($admin){
+        $sql = "insert into admin(nome,login,senha,nivel) values(:nome,:login,:senha,:nivel)";
+        $this->connection->beginTransaction();
+        $stmt = $this->connection->prepare($sql);
+        $stmt->bindParam(":nome",$admin->getNome);
+        $stmt->bindParam(":login",$admin->getLogin());
+        $stmt->bindParam(":senha",$admin->getSenha());
+        $stmt->bindParam(":nivel",$admin->getNivel());
+        $stmt->execute();
+        $id = $this->connection->lastInsertedId();
+        $this->connection->commit();
+        return $id;
+    }
     public function verifica($admin){
         $sql = "select count(*) from admin where login =:login and senha =:senha";
         $stmt = $this->connection->prepare($sql);

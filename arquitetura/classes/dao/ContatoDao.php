80185e6748d6d52ac6feb2bf6a49d793b1f60012
elinaldo.java <elinaldo.java@c1a4cfde-91b5-6961-b273-67d140422e45>
2010-11-09 02:00:02 +0000

    ContatoDao classe para persistir contato - No momento está indo o metódo apenas de insertContato.

---
+++ arquitetura/classes/dao/ContatoDao.php
@@ -0,0 +1,46 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class ContatoDao{
+
+    private $connection = null;
+
+    public function  __construct() {
+
+        $this->connection = Conexao::getConnection();
+    }
+
+    public function insertContato($contato){
+
+        $sql = "insert into contato(cidade,estado,numero,telefoneFixo,telefoneCelular,
+            email,bairro,cep,endereco,complemento,observacao) values (:cidade,:estado,:numero,:telefoneFixo,:telefoneCelular,
+            :email,:bairro,:cep,:endereco,:complemento,:observacao)" ;
+        
+        $this->connection->beginTransaction();
+        $stmt = $this->connection->prepare($sql);
+        
+        $stmt->bindParam(":cidade",$contato->getCidade);
+        $stmt->bindParam(":estado",$contato->getEstado);
+        $stmt->bindParam(":numero",$contato->getNumero);
+        $stmt->bindParam(":telefoneFixo",$contato->getTelefoneFixo);
+        $stmt->bindParam(":telefoneCelular",$contato->getTelefoneCelular);
+        $stmt->bindParam(":email",$contato->getEmail);
+        $stmt->bindParam(":bairro",$contato->getBairro);
+        $stmt->bindParam(":cep",$contato->getCpf);
+        $stmt->bindParam(":endereco",$contato->getEndereco);
+        $stmt->bindParam(":complemento",$contato->getComplemento);
+        $stmt->bindParam(":observacao",$contato->getObservacao);
+
+        $stmt->execute();
+        $id_contato = $this->connection->lastInsertId();
+        $this->connection->commit();
+
+        return $id_contato;
+
+
+    }
+}
+
+?>

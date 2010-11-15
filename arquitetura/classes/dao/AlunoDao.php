ceb2fde4bbd5f13f3011fc47bca507b8e9f371df
elinaldo.java <elinaldo.java@c1a4cfde-91b5-6961-b273-67d140422e45>
2010-11-15 03:51:49 +0000

    CRUD quase completo falta agora só deletar

--- arquitetura/classes/dao/AlunoDao.php
+++ arquitetura/classes/dao/AlunoDao.php
@@ -44,8 +44,63 @@ class AlunoDao{
        return $db = $this->connection->query($sql);
    }
 
+   public function updateAluno(Aluno $aluno){
+
+       $sql = "update aluno,contato,pessoa,pessoafisica set nome =?, sobrenome=?
+           where id_aluno=? and aluno.id_pessoafisica = pessoafisica.id_pessoafisica and
+           pessoafisica.id_pessoa = pessoa.id_pessoa and
+           pessoa.id_contato = contato.id_contato";
+
+      
+       $stmt = $this->connection->prepare($sql);
+
+       $stmt->bindParam(3,$aluno->getId_aluno());
+       $stmt->bindParam(1,$aluno->getNome());
+       $stmt->bindParam(2,$aluno->getSobrenome());
+
+       $stmt->execute();
+      
+   }
+
+       public function selectById($id_aluno){
+
+           $sql = "select * from pessoa,pessoafisica,contato,aluno where id_aluno=? and
+           aluno.id_pessoafisica = pessoafisica.id_pessoafisica and
+           pessoafisica.id_pessoa = pessoa.id_pessoa and
+           pessoa.id_contato = contato.id_contato";
+
+           try {
+
+               $this->connection->beginTransaction();
+               $db = $this->connection->prepare($sql);
+
+
+               if($db->execute(array($id_aluno))){
+
+                   while ($row = $db->fetch()){
+
+                       $aluno = new Aluno();
+                       $aluno->setId_aluno($row["id_aluno"]);
+                       $aluno->setNome($row["nome"]);
+                       $aluno->setSobrenome($row["sobrenome"]);
+                   }
+
+                   return $aluno;
+
+               }
+
+
+           } catch (PDOException $e) {
+               echo $e->getMessage();
+               die();
+           }
+
+    }
+
+
 
 
 
 }
 ?>
+

7e74a9f62485d4e3c8ab827227b5ac438300dd9d
elinaldo.java <elinaldo.java@c1a4cfde-91b5-6961-b273-67d140422e45>
2010-11-15 03:51:49 +0000

    CRUD quase completo falta agora só deletar

---
+++ trunk/arquitetura/editarAluno.php
@@ -0,0 +1,49 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+include 'config/load-classes.php';
+
+if(isset ($_POST["submit"])){
+
+    $aluno = new Aluno;
+
+    $aluno->setId_aluno($_POST["id_aluno"]);
+    $aluno->setNome($_POST["nome"]);
+    $aluno->setSobrenome($_POST["sobrenome"]);
+
+    $alunoDao = new AlunoDao;
+    $alunoDao->updateAluno($aluno);
+
+    header("Location:listarAlunos.php");
+
+}
+
+$showaluno = new AlunoDao();
+$aluno = new Aluno();
+$aluno = $showaluno->selectById($_GET["id_aluno"]);
+
+?>
+
+<html>
+    <head>
+        <title>
+            Editar Alunos
+        </title>
+    </head>
+    <body>
+
+        <form id="form01" name="form01" method="post" action="">
+            
+            <input type="hidden" name="id_aluno" value="<?php echo $aluno->getId_aluno();?>"/>
+            Nome:<input type="text" name="nome" value="<?php echo $aluno->getNome(); ?>"/><br/>
+            Sobrenome:<input type="text" name="sobrenome" value="<?php echo $aluno->getSobrenome(); ?>"/>
+           
+                        <input type="submit" name="submit" value="Atualizar">
+        </form>
+
+
+    </body>
+</html>
--- trunk/arquitetura/listarAlunos.php
+++ trunk/arquitetura/listarAlunos.php
@@ -1,23 +1,49 @@
+<html>
 
+    <head>
+        <meta http-equiv="" content="text/html; charset=utf8"/>
+        <title>
+            Lista de Alunos
+        </title>
+    </head>
+    <body>
+        <h3>Lista de Alunos</h3>
+         <table width="600" border="1" cellpadding="0" cellspacing="0">
+        <tr bgcolor="#CCCCCC">
+            <td width="80" align="left"> ID_ALUNO </td>
+            <td width="242" align="left">NOME </td>
+            <td width="300" align="left">Sobrenome:</td>
+            <td width="350" align="left">Matricula:</td>
+            <td width="400" align="left">Endereço:</td>
+            <td width="450" align="left">Ações:</td>
 
+        </tr>
+        <?php
+        include 'config/load-classes.php';
+        $alunoDao = new AlunoDao();
+        foreach($alunoDao->selectAllAlunos() as $rows){
+        ?>
+
+        <tr>
+        <td><?php echo $rows["id_aluno"];?></td>
+        <td><?php echo $rows["nome"];?></td>
+        <td><?php echo $rows["sobrenome"];?></td>
+        <td><?php echo $rows["matricula"];?></td>
+        <td><?php echo $rows["endereco"];?></td>
+        <td align="center"><a href="editarAluno.php?id_aluno=<?php echo $rows["id_aluno"];?>">
+            [Editar]</a>
+        </tr>
+        
+       
 <?php
-include 'config/load-classes.php';
-
-$alunoDao = new AlunoDao();
-foreach ($alunoDao->selectAllAlunos() as $rows){
-
-
-    echo "Id_aluno:  ".$rows["id_aluno"]."<br/>";
-    echo "Nome:  ".$rows["nome"];
-    echo $rows["sobrenome"]."<br/>";
-    echo "Cidade:  ".$rows["cidade"]."<br/>";
-    echo "Estado:  " .$rows["estado"]."<br/>";
-    echo "Matricula:  " .$rows["matricula"]."<br/>";
-    echo "<br/>-------------------------------------------------------------<br/>";
-
-    
 }
 ?>
+
+     </table>
+    </body>
+</html>
+
+
     
 
         

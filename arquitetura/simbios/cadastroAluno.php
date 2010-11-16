b0181ee0b2a4fff9d62310500d2fb66fec7864c7
camara.denilson@gmail.com <camara.denilson@gmail.com@c1a4cfde-91b5-6961-b273-67d140422e45>
2010-11-16 20:00:48 +0000

    Tela do sistema de controle academico.

---
+++ arquitetura/simbios/cadastroAluno.php
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <title>SIMBIOS - Cadastro de Professores</title>
+        <link rel="stylesheet" type="text/css" href="../media/css/geral.css" />
+        <link rel="stylesheet" type="text/css" href="../media/css/index.css" />
+    </head>
+    <body>
+        <div id="topo">
+
+            <div id="box-logo">
+                <img src="../media/img/logo-simbios.png" id="logo" alt="SIMBIOS" title="SIMBIOS" />
+            </div>
+
+            <div id="header-topo">
+                <span id="saudacao">bem vindo: </span> <strong id="usuario">administrador</strong>
+                <div id="box-info-sistema">
+                    <ul>
+                        <li class="border-right-menu">
+                            <span>Último acesso: </span> <span id="data">01/01/2010 às 05:15:10</span>
+                        </li>
+
+                        <li class="border-left-menu">
+                            <a href="javascript:void(0)">Gerenciar minha conta</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <a href="javascript:void(0)" id="logout">sair</a>
+                
+            </div>
+
+            <div id="box-menu-topo">
+                <ul id="inicio-cadastro">
+                    <li id="inicio">
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li id="atividades">
+                        <a href="javascript:void(0)">histórico de atividades</a>
+                    </li>
+                </ul>
+
+                <ul id="menu-cadastro">
+                    <li>
+                        <a id="cadastro-professor" href="cadastroProfessor.php"><span>cadastrar professor</span></a>
+                    </li>
+                    <li>
+                        <a id="cadastro-aluno" href="cadastroAluno.php"><span>cadastrar aluno</span></a>
+                    </li>
+                </ul>
+                
+                <ul id="menu-pesquisa">
+                    <li>
+                        <a id="pesquisa-professor" href="javascript:void(0)"><span>pesquisar professor</span></a>
+                    </li>
+                    <li>
+                        <a id="pesquisa-aluno" href="javascript:void(0)"><span>pesquisar aluno</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+        <div id="box-corpo">
+            <div id="corpo">
+
+                <h3><strong>Cadastro de Alunos</strong></h3>
+                
+                <div id="box-conteudo">
+                    <div id="menu">
+                        <ul>
+                            <li class="categoria">Cursos</li>
+                            <li class="border-top-none"><a href="javascript:void(0)">Banco de Dados</a></li>
+                            <li><a href="javascript:void(0)">Ciências da Computação</a></li>
+                            <li><a href="javascript:void(0)">Redes de Computadores</a></li>
+                            <li class="border-bottom-none"><a href="javascript:void(0)">Sistemas para Internet</a></li>
+                        </ul>
+                        <ul>
+                            <li class="categoria">Cursos</li>
+                            <li class="border-top-none"><a href="javascript:void(0)">Banco de Dados</a></li>
+                            <li><a href="javascript:void(0)">Ciências da Computação</a></li>
+                            <li><a href="javascript:void(0)">Redes de Computadores</a></li>
+                            <li class="border-bottom-none"><a href="javascript:void(0)">Sistemas para Internet</a></li>
+                        </ul>
+                        <ul>
+                            <li class="categoria">Cursos</li>
+                            <li class="border-top-none"><a href="javascript:void(0)">Banco de Dados</a></li>
+                            <li><a href="javascript:void(0)">Ciências da Computação</a></li>
+                            <li><a href="javascript:void(0)">Redes de Computadores</a></li>
+                            <li class="border-bottom-none"><a href="javascript:void(0)">Sistemas para Internet</a></li>
+                        </ul>
+                    </div>
+
+                    <div id="conteudo">
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="rodape">
+            
+        </div>
+        
+    </body>
+</html>

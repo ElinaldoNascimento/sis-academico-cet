893ae41c4858ee89cbc3537b4568d6b28f0dc9b2
camara.denilson@gmail.com <camara.denilson@gmail.com@c1a4cfde-91b5-6961-b273-67d140422e45>
2010-11-29 19:53:02 +0000

    Tela do sistema de controle academico.

--- arquitetura/simbios/cadastroProfessor.php
+++ arquitetura/simbios/cadastroProfessor.php
@@ -1,10 +1,11 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pt-br" lang="pt-br">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
         <title>SIMBIOS - Cadastro de Professores</title>
         <link rel="stylesheet" type="text/css" href="../media/css/geral.css" />
-        <link rel="stylesheet" type="text/css" href="../media/css/index.css" />
+        <link rel="stylesheet" type="text/css" href="../media/css/cadastro-professor.css" />
     </head>
     <body>
         <div id="topo">
@@ -93,14 +94,113 @@
 
                     <div id="conteudo">
 
+                        <form action="cadastroAluno.php" method="post" name="formCadastroAluno">
 
+                            <fieldset>
+                                <legend id="grupo-dados-pessoais"><img src="../media/img/seta.png" alt="" /> Dados pessoais </legend>
+                                <p>
+                                <label for="nome">Nome:</label><input type="text" name="nome" id="nome" />
+                                </p>
+                                <p>
+                                    <label for="sobrenome">SobreNome:</label><input type="text" name="sobrenome" id="sobrenome" />
+                                </p>
+                            </fieldset>
+
+                            <fieldset>
+                                <legend id="grupo-endereco"><img src="../media/img/seta.png" alt="" /> Endereço </legend>
+                                <p>
+                                    <label for="endereco">Endereço:</label><input type="text" name="endereco" id="endereco" />
+                                </p>
+                                <p>
+                                    <label for="bairro">Bairro:</label><input type="text" name="bairro" id="bairro" />
+                                </p>
+                                <p>
+                                    <label for="complemento">Complemento:</label><input type="text" name="complemento" id="complemento" />
+                                </p>
+                                <p>
+                                    <label for="cidade">Cidade:</label><input type="text" name="cidade" id="cidade" />
+                                </p>
+                                <p>
+                                    <label for="estado">Estado:</label><input type="text" name="estado" id="estado" />
+                                </p>
+                                <p>
+                                    <label for="numero">Numero:</label><input type="text" name="numero" id="numero" />
+                                </p>
+                                <p>
+                                    <label for="cep">CEP:</label><input type="text" name="cep" id="cep" />
+                                </p>
+                            </fieldset>
+
+                            <fieldset>
+                                <legend id="grupo-dados-academicos"><img src="../media/img/seta.png" alt="" /> Dados acadêmicos </legend>
+                                <p>
+                                    <label for="email">E-Mail:</label><input type="text" name="email" id="email" />
+                                </p>
+                                <p>
+                                    <label for="telefonefixo">Telefone Fixo:</label><input type="text" name="telefoneFixo" id="telefonefixo" />
+                                </p>
+                                <p>
+                                    <label for="telefonecelular">Telefone Celular:</label><input type="text" name="telefoneCelular" id="telefonecelular" />
+                                </p>
+                                <p>
+                                    <label for="obervacao">Observação:</label><input type="text" name="observacao" id="observacao" />
+                                </p>
+                                <p>
+                                    <label for="cpf">Cpf:</label><input type="text" name="cpf" id="cpf" />
+                                </p>
+                                <p>
+                                    <label for="datanascimento">DataNascimento:</label><input type="text" name="dataNascimento" id="datanascimento" />
+                                </p>
+                                <p>
+                                    <label for="estadocivil">EstadoCivil:</label><input type="text" name="estadoCivil" id="estadocivil" />
+                                </p>
+                                <p>
+                                    <label for="estadoexpedidor">EstadoExpedidor:</label><input type="text" name="estadoExpedidor" id="estadoexpedidor" />
+                                </p>
+                                <p>
+                                    <label for="nacionalidade">Nacionalidade:</label><input type="text" name="nacionalidade" id="nacionalidade" />
+                                </p>
+                                <p>
+                                    <label for="naturalidade">Naturalidade:</label><input type="text" name="naturalidade" id="naturalidade" />
+                                </p>
+                                <p>
+                                    <label for="orgaoexpedidor">OrgaoExpedidor:</label><input type="text" name="orgaoExpedidor" id="orgaoExpedidor" />
+                                </p>
+                                <p>
+                                    <label for="rg">RG:</label><input type="text" name="rg" id="rg" />
+                                </p>
+                                <p>
+                                    <label for="sexo">Sexo:</label><input type="text" name="sexo" id="sexo" />
+                                </p>
+                                <p>
+                                    <label for="matricula">Matricula:</label><input type="text" name="matricula" id="matricula" />
+                                </p>
+                            </fieldset>
+
+                            <p>
+                                <input type="submit" value=".:Enviar:."/>
+                            </p>
+
+                            <p>
+                                <input type="reset" value=".:Limpar:."/>
+                            </p>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
 
         <div id="rodape">
-            
+            <p>
+                <a href="http://jigsaw.w3.org/css-validator/check/referer">
+                    <img src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS válido!" />
+                </a>
+            </p>
+            <p>
+                <a href="http://validator.w3.org/check?uri=referer">
+                    <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" />
+                </a>
+            </p>
         </div>
         
     </body>
